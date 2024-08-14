566cb82801a16169f37109c62cfdd745d282fdfc
Norman Koch <norman.koch@tu-dresden.de>
2024-08-14 09:49:49 +0200

    fix

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="sidebar.css">
+    <link rel="stylesheet" href="navigation.css">
+    <link rel="stylesheet" href="stylesheet.css">
+    <meta charset="UTF-8">
+    <title>Start Screen</title>
+    <script src="https://asanai.scads.ai/asanai.js.php"></script>
+    <script src="script.js"></script>
+</head>
+<body>
