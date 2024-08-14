c1e2e65a8b0cde88ea04ebeb10dec7ecf4deed16
Norman Koch <norman.koch@tu-dresden.de>
2024-08-14 09:42:12 +0200

    fix

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <div class="general">
     </div>
     <div class="start">
-        <video autoplay loop muted onclick="location.href='01_StartScreen.html'">
+        <video autoplay loop muted onclick="location.href='01_StartScreen.php'">
             <source type="video/webm" src="media/images/fcnn_animation_new_2.mp4">
         </video>
     </div>
