98072e15d947ed1cf8197b6daf90f4a6d82276be
Norman Koch <norman.koch@tu-dresden.de>
2024-08-14 09:41:11 +0200

    fix

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="">
+<head>
+    <link rel="stylesheet" href="navigation.css">
+    <link rel="stylesheet" href="stylesheet.css">
+    <meta charset="UTF-8">
+    <title>NeuralNetworksExplained</title>
+    <script src="https://asanai.scads.ai/asanai.js.php"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+<div class="groeße">
+    <div class="general">
+    </div>
+    <div class="start">
+        <video autoplay loop muted onclick="location.href='01_StartScreen.html'">
+            <source type="video/webm" src="media/images/fcnn_animation_new_2.mp4">
+        </video>
+    </div>
+</div>
+</body>
+</html>
