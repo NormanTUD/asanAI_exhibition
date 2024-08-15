26c99fb5efbac34a743ddc65fca92941ec7df1cb
Norman Koch <norman.koch@tu-dresden.de>
2024-08-15 10:03:07 +0200

    fix

--- 10_SelectDatasetScreen.php
+++ 10_SelectDatasetScreen.php
@@ -3,20 +3,17 @@
 	include("header.php");
 ?>
 <div class="general">
-    <p>Choose a use-case or train <br> your own dataset</p>
-    <div class="container">
-        <button id="box1" class="box" onclick="location.href='11_Test_Images.php'">Obst</button>
-
-        <button id="box2" class="box" onclick="location.href='12_Option2.php'">option2</button>
-
-        <button id="box3" class="box" onclick="location.href='30_Train.php'">create dataset</button>
-
-    </div>
+	<p>Wähle einen existierenden Datensatz <br> oder erstelle deinen Eigenen</p>
+	<div class="container">
+		<button id="box1" class="box" onclick="location.href='11_Test_Images.php'">Obst</button>
+		<button id="box2" class="box" onclick="location.href='12_Option2.php'">option2</button>
+		<button id="box3" class="box" onclick="location.href='30_Train.php'">Eigener Datensatz</button>
+	</div>
 </div>
 <div class="navbar" id="only_1">
-    <ul>
-        <li><a class="icon" href="index.php"><img src="media/images/Home_icon.svg"></a></li>
-    </ul>
+	<ul>
+		<li><a class="icon" href="index.php"><img src="media/images/Home_icon.svg"></a></li>
+	</ul>
 </div>
 </body>
 </html>
