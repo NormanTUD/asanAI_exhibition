087fad78af9c3ff45d61b2e105f9b69c15f8caed
Norman Koch <norman.koch@tu-dresden.de>
2024-08-15 18:23:02 +0200

    new version

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function assert(cond, msg) {
 var log = console.log;
 
 var nr_epochs = 100;
-var max_nr_images = 20;
+var max_nr_images = 10;
 var batch_size = 200;
 var __categories = ["Apfel", "Banane", "Orange"];
 
