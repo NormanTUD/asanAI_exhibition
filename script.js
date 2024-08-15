61ccdc3bb05c3c5d3dca4cb18301372d890828a8
Norman Koch <norman.koch@tu-dresden.de>
2024-08-15 18:05:23 +0200

    new version

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function assert(cond, msg) {
 
 var log = console.log;
 
-var nr_epochs = 100;
-var max_nr_images = 20;
+var nr_epochs = 200;
+var max_nr_images = 40;
 var batch_size = 200;
 var __categories = ["Apfel", "Banane", "Orange"];
 
@@ -36,6 +36,7 @@ var model_struct = [
 	{maxPooling2d: {poolSize: [3, 3] }},
 	{conv2d: {filters: 2, activation: "tanh", kernelInitializer: _kernel_initializer, biasInitializer: _bias_initializer, kernelSize: [3, 3] }},
 	{maxPooling2d: {poolSize: [3, 3] }},
+	{conv2d: {filters: 2, activation: "tanh", kernelInitializer: _kernel_initializer, biasInitializer: _bias_initializer, kernelSize: [3, 3] }},
 	{flatten: {}},
 	{dense: {units: 4, activation: "tanh", kernelInitializer: _kernel_initializer, biasInitializer: _bias_initializer}},
 	{dense: {units: 8, activation: "tanh", kernelInitializer: _kernel_initializer, biasInitializer: _bias_initializer}},
