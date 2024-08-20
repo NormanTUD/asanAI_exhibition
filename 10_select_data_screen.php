879d217090b636568ecc672d8b1e2b8f29e6f42a
Norman Koch <norman.koch@tu-dresden.de>
2024-08-20 11:07:21 +0200

    fix

--- 10_select_data_screen.php
+++ 10_select_data_screen.php
@@ -2,18 +2,18 @@
 	$GLOBALS["use_navigation"] = 1;
 	include("header.php");
 ?>
-<div class="general">
-	<p style="margin: 200px;">Wähle einen existierenden Datensatz oder erstelle deinen Eigenen</p>
-	<div class="container">
-		<button id="box1" class="box" onclick="location.href='11_test_images.php'">Obst</button>
-		<button id="box2" class="box" onclick="location.href='12_option2.php'">Alltags&shy;gegenstände</button>
-		<button id="box3" class="box" onclick="location.href='30_train.php'">Eigener Datensatz</button>
-	</div>
-</div>
-<div class="navbar" id="only_1">
-	<ul>
-		<li><a class="icon" href="index.php"><img src="media/images/Home_icon.svg"></a></li>
-	</ul>
-</div>
-</body>
+		<div class="general">
+			<p style="margin: 200px;">Wähle einen existierenden Datensatz oder erstelle deinen Eigenen</p>
+			<div class="container">
+				<button id="box1" class="box" onclick="location.href='11_test_images.php'">Obst</button>
+				<button id="box2" class="box" onclick="location.href='12_option2.php'">Alltags&shy;gegenstände</button>
+				<button id="box3" class="box" onclick="location.href='30_train.php'">Eigener Datensatz</button>
+			</div>
+		</div>
+		<div class="navbar" id="only_1">
+			<ul>
+				<li><a class="icon" href="index.php"><img src="media/images/Home_icon.svg"></a></li>
+			</ul>
+		</div>
+	</body>
 </html>
