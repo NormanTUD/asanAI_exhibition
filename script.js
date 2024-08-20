01ba5788447120822bbdcf270196774e02d7ea42
Norman Koch <norman.koch@tu-dresden.de>
2024-08-20 13:29:55 +0200

    fix

--- script.js
+++ script.js
@@ -470,19 +470,19 @@ function matrix_texts(){
 function initialize_keyboard() {
 	keyboard = window.SimpleKeyboard.default;
 
-	const myKeyboard = new keyboard({
-		onChange: input => onChange(input),
-		onKeyPress: button => onKeyPress(button)
-	});
 
 	function onChange(input) {
-		document.querySelector(".input").value = input;
 		console.log("Input changed", input);
 	}
 
 	function onKeyPress(button) {
 		console.log("Button pressed", button);
 	}
+
+	const myKeyboard = new keyboard({
+		onChange: input => onChange(input),
+		onKeyPress: button => onKeyPress(button)
+	});
 }
 
 function hide_keyboard () {
