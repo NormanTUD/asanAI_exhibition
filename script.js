0b7a95157b80d7711ea9d7293bc50f0332e3abf9
Norman Koch <norman.koch@tu-dresden.de>
2024-08-20 13:40:53 +0200

    fix

--- script.js
+++ script.js
@@ -479,14 +479,46 @@ function initialize_keyboard(elem) {
 		console.log("Button pressed", button);
 
 		if(button == "bksp") {
-			log("remove last");
 			elem.value = elem.value.substring(0, elem.value.length - 1);
 		}
 	}
 
 	const myKeyboard = new keyboard({
 		onChange: input => onChange(input),
-		onKeyPress: button => onKeyPress(button)
+		onKeyPress: button => onKeyPress(button),
+		mergeDisplay: true,
+		layoutName: "default",
+		layout: {
+			default: [
+				"q w e r t y u i o p",
+				"a s d f g h j k l",
+				"{shift} z x c v b n m {backspace}",
+				"{numbers} {space} {ent}"
+			],
+			shift: [
+				"Q W E R T Y U I O P",
+				"A S D F G H J K L",
+				"{shift} Z X C V B N M {backspace}",
+				"{numbers} {space} {ent}"
+			],
+			numbers: ["1 2 3", "4 5 6", "7 8 9", "{abc} 0 {backspace}"]
+		},
+		display: {
+			"{numbers}": "123",
+			"{ent}": "return",
+			"{escape}": "esc ⎋",
+			"{tab}": "tab ⇥",
+			"{backspace}": "⌫",
+			"{capslock}": "caps lock ⇪",
+			"{shift}": "⇧",
+			"{controlleft}": "ctrl ⌃",
+			"{controlright}": "ctrl ⌃",
+			"{altleft}": "alt ⌥",
+			"{altright}": "alt ⌥",
+			"{metaleft}": "cmd ⌘",
+			"{metaright}": "cmd ⌘",
+			"{abc}": "ABC"
+		}
 	});
 }
 
