2d777fcf8a2a356e9c0be56b30469e55e876bf7a
Norman Koch <norman.koch@tu-dresden.de>
2024-08-20 15:03:51 +0200

    fix

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 	$GLOBALS["dont_load_asanai"] = 1;
 	include("header.php");
 ?>
-<div>
-    <div class="general">
-    </div>
-    <div class="start">
-        <video autoplay loop muted onclick="location.href='01_start_screen.php'">
-            <source type="video/webm" src="media/images/fcnn_animation_new_2.mp4">
-        </video>
-    </div>
-</div>
-</body>
+		<div class="general">
+			</div>
+				<div class="start">
+					<video autoplay loop muted onclick="location.href='01_start_screen.php'">
+						<source type="video/webm" src="media/images/fcnn_animation_new_2.mp4">
+					</video>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
