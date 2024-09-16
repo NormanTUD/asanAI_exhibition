9064f26a1c5d5d131e7e6c202389dee67e205108
Norman Koch <norman.koch@tu-dresden.de>
2024-09-16 10:05:36 +0200

    fix

--- 10_select_data_screen.php
+++ 10_select_data_screen.php
@@ -5,17 +5,19 @@
 		<div id="optimizer_div" style="display: none;"></div>
 		<div class="general">
 		<div class="asanai_logo"><img class="asanai_img" src="media/images/logo_small_dark.png" /></div>
-			<h2 style="margin: 200px;">Wähle einen existierenden Datensatz oder erstelle deinen Eigenen</h2>
-			<div class="container">
-				<button id="box1" class="box inverted" onclick="location.href='11_test_images.php'">Obst</button>
-				<button id="box2" class="box inverted" onclick="location.href='12_option2.php'">Alltags&shy;gegenstände</button>
-				<button id="box3" class="box inverted" onclick="location.href='30_train.php'">Eigener Datensatz</button>
-			</div>
+		<h2 style="margin: 200px;"><span class="TRANSLATEME_choose_an_existing_dataset_or_create_your_own"></span></h2>
+		<div class="container">
+			<button id="box1" class="box inverted" onclick="location.href='11_test_images.php'">
+				<span class="TRANSLATEME_fruit"></span>
+			</button>
+			<button id="box2" class="box inverted" onclick="location.href='12_option2.php'">
+				<span class="TRANSLATEME_everyday_objects"></span>
+			</button>
+			<button id="box3" class="box inverted" onclick="location.href='30_train.php'">
+				<span class="TRANSLATEME_own_dataset"></span>
+			</button>
 		</div>
-		<div class="button shadow b_topRight" id="b_en" onclick="switchLanguage('en')">
-			<div class="language_en"></div>
-		</div>
-		<div class="button shadow b_topRight hidden" id="b_de" onclick="switchLanguage('de')">
+		<div class="button shadow b_topRight hidden" id="b_de" onclick="switch_language()">
 			<div class="language_de"></div>
 		</div>
 		<div class="button shadow b_topRight2" onclick="closeInfopanel()">
@@ -24,5 +26,7 @@
 	</body>
 	<script>
 		showSpinnerFullScreen();
+
+		update_translations();
 	</script>
 </html>
