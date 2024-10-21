861f3d095995ad5822c75ff9e32180735cf55955
Norman Koch <norman.koch@tu-dresden.de>
2024-10-21 10:50:32 +0200

    fix

--- script.js
+++ script.js
@@ -140,6 +140,7 @@ $(document).ready(async function() {
 	asanai.set_validation_split(0);
 
 	asanai.set_rescale_factor(3);
+	asanai.set_layer_spacing_add(30);
 
 	$("#nr_epochs").html(nr_epochs);
 	$("#progress").attr("max", nr_epochs);
