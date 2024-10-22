34c7bb61af44cfeb6698c7954ee13346579a030d
Norman Koch <norman.koch@tu-dresden.de>
2024-10-22 14:14:10 +0200

    fix

--- startWebserver.sh
+++ startWebserver.sh
@@ -1,5 +1,10 @@
-kill $(lsof -t -i:8999)
+#!/bin/bash
+
+for running_proc in $(lsof -t -i:8999); do
+	kill $running_proc
+done
 # local server 
-cd ~/asanai_exhibition
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+cd $SCRIPT_DIR
 php -S 127.0.0.1:8999
 sleep 5
